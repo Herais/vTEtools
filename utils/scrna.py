7531733b1042f992ee8ccd0ffd11c1633970abea
Herais <vee.xu.x@gmail.com>
2024-05-08 15:52:20 -0700

    refactor: Refactor import statements in SCRNA class

--- utils/scrna.py
+++ utils/scrna.py
@@ -28,7 +28,6 @@ class SCRNA(object):
         data = sp.sparse.csr_matrix(data.to_numpy())
         data.astype('float32')
         
-        print('Loaded {0}'.format(name))
         ad = AnnData(data, obs={'obs_names': cells}, var={'var_names': features})
         del data
         return ad
