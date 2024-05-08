d8976fb92662e2da7898642f4072ca8c8c54a8d6
Herais <vee.xu.x@gmail.com>
2024-05-07 23:45:19 -0700

    add dna module

---
+++ utils/dna.py
@@ -0,0 +1,14 @@
+import Bio
+from Bio import SeqIO
+from Bio.Seq import Seq
+
+class DNA(object):
+ 
+    def __init__(self):
+        """
+        Parameters
+        ----------
+        size : float
+            radiu
+        """
+        super(DNA, self).__init__()
