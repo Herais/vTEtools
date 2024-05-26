39d59e3a62f1a3eb42ba43ba703fcb972c04c284
Herais <vee.xu.x@gmail.com>
2024-05-26 11:57:35 -0700

    updated2

---
+++ utils/annotation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+
+class ANNOTATION(object):
+    """
+    """
+
+    def __init__(self):
+        """
+        Parameters
+        ----------
+
+        """
+        super(ANNOTATION, self).__init__()
