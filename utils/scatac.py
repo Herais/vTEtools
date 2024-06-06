1f77ef77c5fad8047fdb42a5f940508135464fd2
Herais <vee.xu.x@gmail.com>
2024-06-05 17:02:03 -0700

    refactor: Refactor process_df_to_scanpy method in SCATAC class
    The code changes in `utils/scatac.py` refactor the `process_df_to_scanpy` method in the SCATAC class. The method now filters columns for TE(s) and marker(s) and converts the data to a sparse matrix using `scipy.sparse.csr_matrix`. This improves the efficiency and functionality of the method for processing and analyzing single-cell ATAC-seq datasets.

--- utils/scatac.py
+++ utils/scatac.py
@@ -18,23 +18,17 @@ class SCATAC(object):
     @staticmethod
     def process_df_to_scanpy(df):
 
-        # filter columns for TE(s) and marker(s)    
+        # filter columns for TE(s) and marker(s)
         data = df.copy()
         features = data.columns
         cells = data.index
-            
+
+        import scipy as sp
         data = sp.sparse.csr_matrix(data.to_numpy())
         data.astype('float32')
 
-        '''
-        oh = open('gene_names.{0}.tsv'.format(os.path.split(filename)[1]), 'w')
-        for g in genes:
-            oh.write('%s\n' % g)
-        oh.close()
-        '''
-        
-        print('Loaded {0}'.format(name))
-        ad = AnnData(data, obs={'obs_names': cells}, var={'var_names': features})
+        ad = AnnData(data, obs={'obs_names': cells},
+                     var={'var_names': features})
         del data
         return ad
     
