dd84c38778b9a4697b4d34e92da2ec4bd11f86f6
Herais <vee.xu.x@gmail.com>
2024-07-10 16:50:28 -0700

    updated

--- utils/sequence.py
+++ utils/sequence.py
@@ -34,6 +34,29 @@ class DNA(object):
 
         return ls_seq
     
+    @staticmethod
+    def get_tmpfp_bed4(S, fp="./tmp.bed"):
+    
+        df_coord = S.str.split('[:-]', expand=True)
+        df_coord.columns = ['Chromosome', 'Start', 'End']
+        df_coord['repName'] = df_coord.index
+        
+        bed = pybed.BedFrame.from_frame(meta=[], data=df_coord)
+        bed.to_file(fp)
+        return fp
+
+    @staticmethod
+    def get_flop_coordinates(S, fp_genome:str, len_flop=50):
+        
+        fp_bed = get_tmpfp_bed4(S)
+        cmd = "bedtools slop -b {} -i {} -g {}".format(len_flop, fp_bed, fp_genome)
+        res = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        df_flop_coord = pd.read_csv(StringIO(res.stdout), sep='\t', header=None).set_index(3).sort_index()
+        df_flop_coord.index.names = ['names']
+
+        df_flop_coord.columns = [['meta']*3, ['chr_flop', 'start_flop', 'end_flop']]
+        return df_flop_coord
+    
 
 class RNA(object):
     """
