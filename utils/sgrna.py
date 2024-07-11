cadc35fe5df860138c53573cf19e7e92a9e3e2ae
Herais <vee.xu.x@gmail.com>
2024-07-10 17:22:56 -0700

    added utils.scgrna

---
+++ utils/sgrna.py
@@ -0,0 +1,17 @@
+import Bio
+from Bio import Entrez, SeqIO
+from Bio.Seq import Seq
+from fuc import pybed
+
+
+class SGRNA(object):
+    """
+    """
+
+    def __init__(self):
+        """
+        Parameters
+        ----------
+
+        """
+        super(SGRNA, self).__init__()
